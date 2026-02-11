440d571c0e2893be40074ace2341a71b6bb96e19
kowshika1912 <kowshika1912@gmail.com>
2026-02-11 13:34:07 +0530

    images

--- src/pages/home/foundersection.jsx
+++ src/pages/home/foundersection.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 // --- ASSETS ---
 // Swapped for a professional "Founder" style portrait
-const founderImage = "public/assets/Founder_Pic.png";
+const founderImage = "/public/assets/Founder_Pic.png";
 
 
 // --- REUSABLE COMPONENTS ---
